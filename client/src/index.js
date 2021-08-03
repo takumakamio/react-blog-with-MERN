80ad0ab671846e093c39b355187049417c73b7ce
神尾拓馬 <balo@MacBook-Pro.local>
2021-08-03 22:15:34 +0900

    index.js

--- client/src/index.js
+++ client/src/index.js
@@ -4,10 +4,10 @@ import App from "./App";
 import { ContextProvider } from "./context/Context";
 
 ReactDOM.render(
-  <>
+  <React.StrictMode>
     <ContextProvider>
       <App />
     </ContextProvider>
-  </>,
+  </React.StrictMode>,
   document.getElementById("root")
 );
