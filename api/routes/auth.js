2c5a167e38e75f2400af72d6f44cb1b87d702d1b
神尾拓馬 <balo@MacBook-Pro.local>
2021-09-01 04:45:34 +0900

    JWT

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,36 +1,100 @@
 const router = require("express").Router();
 const User = require("../models/User");
-const bcrypt = require("bcrypt");
+const CryptoJS = require("crypto-js");
+const jwt = require("jsonwebtoken");
+const verify = require("../verifyToken");
 
-//REGISTER
-router.post("/register", async (req, res) => {
-  try {
-    const salt = await bcrypt.genSalt(10);
-    const hashedPass = await bcrypt.hash(req.body.password, salt);
-    const newUser = new User({
-      username: req.body.username,
-      email: req.body.email,
-      password: hashedPass,
-    });
+let refreshTokens = [];
 
-    const user = await newUser.save();
-    res.status(200).json(user);
-  } catch (err) {
-    res.status(500).json(err);
+const generateAccessToken = (user) => {
+  return jwt.sign(
+    { id: user.id, username: user.username },
+    process.env.SECRET_KEY,
+    {
+      expiresIn: "5s",
+    }
+  );
+};
+
+const generateRefreshToken = (user) => {
+  return jwt.sign(
+    { id: user.id, username: user.username },
+    process.env.REFRESH_SECRET_KEY
+  );
+};
+
+// MAKE REFRESH TOKEN
+router.post("/refresh", (req, res) => {
+  //take the refresh token from the user
+  const refreshToken = req.body.token;
+
+  //send error if there is no token or it's invalid
+  if (!refreshToken) return res.status(401).json("You are not authenticated!");
+  if (!refreshTokens.includes(refreshToken)) {
+    return res.status(403).json("Refresh token is not valid!");
   }
+  jwt.verify(refreshToken, process.env.REFRESH_SECRET_KEY, (err, user) => {
+    err && console.log(err);
+    refreshTokens = refreshTokens.filter((token) => token !== refreshToken);
+
+    const newAccessToken = generateAccessToken(user);
+    const newRefreshToken = generateRefreshToken(user);
+
+    refreshTokens.push(newRefreshToken);
+
+    res.status(200).json({
+      accessToken: newAccessToken,
+      refreshToken: newRefreshToken,
+    });
+  });
+
+  //if everything is ok, create new access token, refresh token and send to user
 });
 
 //LOGIN
 router.post("/login", async (req, res) => {
   try {
     const user = await User.findOne({ username: req.body.username });
-    !user && res.status(400).json("Wrong credentials!");
+    !user && res.status(401).json("Wrong password or username!");
 
-    const validated = await bcrypt.compare(req.body.password, user.password);
-    !validated && res.status(400).json("Wrong credentials!");
+    const bytes = CryptoJS.AES.decrypt(user.password, process.env.SECRET_KEY);
+    const originalPassword = bytes.toString(CryptoJS.enc.Utf8);
 
-    const { password, ...others } = user._doc;
-    res.status(200).json(others);
+    originalPassword !== req.body.password &&
+      res.status(401).json("Wrong password or username!");
+
+    if (user) {
+      //Generate an access token
+      const accessToken = generateAccessToken(user);
+      const refreshToken = generateRefreshToken(user);
+      refreshTokens.push(refreshToken);
+    }
+    const { password, ...info } = user._doc;
+    res.status(200).json({ ...info, accessToken, refreshToken });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.post("/logout", verify, (req, res) => {
+  const refreshToken = req.body.token;
+  refreshTokens = refreshTokens.filter((token) => token !== refreshToken);
+  res.status(200).json("You logged out successfully.");
+});
+
+//REGISTER
+router.post("/register", async (req, res) => {
+  const newUser = new User({
+    username: req.body.username,
+    email: req.body.email,
+    password: CryptoJS.AES.encrypt(
+      req.body.password,
+      process.env.SECRET_KEY
+    ).toString(),
+  });
+  try {
+    const user = await newUser.save();
+    res.status(201).json(user);
   } catch (err) {
     res.status(500).json(err);
   }
